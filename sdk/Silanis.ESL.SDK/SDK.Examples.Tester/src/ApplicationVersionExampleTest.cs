192648cbb5151d14a7e97b4e21de8f496fd74ce7
psmelser <paul.smelser@gmail.com>
2016-04-08 20:18:53 -0400

    working on deployment items

--- sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/ApplicationVersionExampleTest.cs
+++ sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/ApplicationVersionExampleTest.cs
@@ -4,15 +4,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SDK.Examples
 {
     [TestClass]
-    [DeploymentItem("signers.json", "src")]
-    [DeploymentItem("prêt.pdf", "src")]
-    [DeploymentItem("document.pdf", "src")]
-    [DeploymentItem("document-for-anchor-extraction.pdf", "src")]
-    [DeploymentItem("document-with-fields.pdf", "src")]
-    [DeploymentItem("extract_document.pdf", "src")]
-    [DeploymentItem("field_groups.pdf", "src")]
-    [DeploymentItem("document.odt", "src")]
-    [DeploymentItem("document.rtf", "src")]
+    [DeploymentItem("signers.json")]
+    [DeploymentItem("prêt.pdf")]
+    [DeploymentItem("document.pdf")]
+    [DeploymentItem("document-for-anchor-extraction.pdf")]
+    [DeploymentItem("document-with-fields.pdf")]
+    [DeploymentItem("extract_document.pdf")]
+    [DeploymentItem("field_groups.pdf")]
+    [DeploymentItem("document.odt")]
+    [DeploymentItem("document.rtf")]
     public class ApplicationVersionExampleTest
     {
         [TestMethod]
