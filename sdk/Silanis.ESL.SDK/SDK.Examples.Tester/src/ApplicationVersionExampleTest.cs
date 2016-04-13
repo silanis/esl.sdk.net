b42823cfe1dd72cdd53c0665c0f9a14a7941b3d9
HouJianyu <intersun@msn.com>
2016-04-13 19:07:08 -0400

    Changed the way of deploymentItem

--- sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/ApplicationVersionExampleTest.cs
+++ sdk/Silanis.ESL.SDK/SDK.Examples.Tester/src/ApplicationVersionExampleTest.cs
@@ -3,16 +3,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SDK.Examples
 {
-    [DeploymentItem("../signers.json", "/")]
-    [DeploymentItem("../prêt.pdf", "/src/")]
-    [DeploymentItem("../document.pdf", "/src/")]
-    [DeploymentItem("../document-for-anchor-extraction.pdf", "/src/")]
-    [DeploymentItem("../document-with-fields.pdf", "/src/")]
-    [DeploymentItem("../extract_document.pdf", "/src/")]
-    [DeploymentItem("../field_groups.pdf", "/src/")]
-    [DeploymentItem("../document.odt", "/src/")]
-    [DeploymentItem("../document.rtf", "/src/")]
     [TestClass]
+    [DeploymentItem("signers.json", "src")]
+    [DeploymentItem("prêt.pdf", "src")]
+    [DeploymentItem("document.pdf", "src")]
+    [DeploymentItem("document-for-anchor-extraction.pdf", "src")]
+    [DeploymentItem("document-with-fields.pdf", "src")]
+    [DeploymentItem("extract_document.pdf", "src")]
+    [DeploymentItem("field_groups.pdf", "src")]
+    [DeploymentItem("document.odt", "src")]
+    [DeploymentItem("document.rtf", "src")]
     public class ApplicationVersionExampleTest
     {
         [TestMethod]
